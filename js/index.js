2b5d822a93361078c0d9355df5700b3ddaf5e16e
bruce-zxy <bruce_zxy@163.com>
2017-11-01 04:40:20 +0800

    add shoppingCart.html

--- js/index.js
+++ js/index.js
@@ -20,3 +20,22 @@ $(".classify_nav a").click(function () {
 	$(".classify_nav a").removeClass('active');
 	$(this).addClass('active');
 })
+
+$(".cart_item img").click(function () {
+	$(this).prev().toggleClass("selected");
+})
+
+$(".cart_item .add").click(function () {
+	var count = $(this).prev().html()*1;
+	var price = $(this).parent().prev().find(".price").html()*1;
+	$(this).prev().html(++count);
+	$(this).parent().find(".sum").html(count*price);
+})
+
+$(".cart_item .sub").click(function () {
+	var count = $(this).next().html()*1;
+	var price = $(this).parent().prev().find(".price").html()*1;
+	if (count === 1) return
+	$(this).next().html(--count);
+	$(this).parent().find(".sum").html(count*price);
+})
